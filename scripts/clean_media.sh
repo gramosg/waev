7e9586e84f5dba0c40d5181c27e73422259e8161
Guillermo Ramos <gramos@gramos.me>
2020-02-06 15:30:30 +0100

    Fix commented code

--- scripts/clean_media.sh
+++ scripts/clean_media.sh
@@ -8,8 +8,8 @@ fi
 MEDIA_DIR=$1
 CHAT_TXT_PATH=$2
 
-# find "$MEDIA_DIR" -mindepth 2 -type f '!' -name '.nomedia' -exec mv -t "$MEDIA_DIR" -i '{}' +
-# find "$MEDIA_DIR" -mindepth 1 -maxdepth 1 -type d -exec rm -rf '{}' \;
+find "$MEDIA_DIR" -mindepth 2 -type f '!' -name '.nomedia' -exec mv -t "$MEDIA_DIR" -i '{}' +
+find "$MEDIA_DIR" -mindepth 1 -maxdepth 1 -type d -exec rm -rf '{}' \;
 for f in *; do
     if grep "$f" "$CHAT_TXT_PATH" > /dev/null; then
         echo "+$f"
