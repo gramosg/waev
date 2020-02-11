abad279fd6247b81e04efd9110ffe221bb096708
Guillermo Ramos <gramos@gramos.me>
2020-02-11 17:08:42 +0100

    CSS cleanup

--- lib/waev_web/views/exports_view.ex
+++ lib/waev_web/views/exports_view.ex
@@ -1,3 +1,25 @@
 defmodule WaevWeb.ExportsView do
   use WaevWeb, :view
+
+  def party_peek(assigns, party) do
+    ~E"""
+<div class="party-peek">
+  <figure>
+    <%= party_avatar(assigns, party, :big) %>
+    <figcaption><%= party.name %></figcaption>
+  </figure>
+</div>
+"""
+  end
+
+  def party_avatar(assigns, party, size) do
+    modifier =
+      case size do
+        :tiny -> "avatar--tiny"
+        :big -> "avatar--big"
+      end
+    ~E"""
+<img class="avatar <%= modifier %>" src="<%= Routes.exports_path(@conn, :get_avatar, @id, party.name) %>" />
+"""
+  end
 end
